d0358c93993cba551a3a865a0019a81fc99fac55
melvin <ianndiormel@gmail.com>
2025-01-07 23:03:27 +0100

    updated the styles in the Hero file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import Logo from '../assets'
+import Logo from '../assets/logo.svg'
 
 export default function Hero() {
     return (
@@ -9,7 +9,7 @@ export default function Hero() {
                 <button
                 type='button'
                 onClick={() => window.open()}
-                className='balck_btn'
+                className='black_btn'
                 >
                     Github
                 </button>
@@ -17,7 +17,7 @@ export default function Hero() {
 
             <h1 className='head_text'>
                 Summarize Article with <br className='max-md:hidden'/>
-                <span className='orange _gradient'>OpenAi GPT-4</span>
+                <span className='orange_gradient'>OpenAi GPT-4</span>
             </h1>
             <h2 className='desc'>
                 simplify your reading with summize,an open-source article summarizer that transforms lengthy articles into clear and concise summaries 
