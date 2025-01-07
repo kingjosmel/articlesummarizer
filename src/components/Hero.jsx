4dc19585f106084eb00905223dc3b57c9f9ab0d4
melvin <ianndiormel@gmail.com>
2025-01-07 22:56:10 +0100

    first commit, commpleted the hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import Logo from '../assets'
+
+export default function Hero() {
+    return (
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+                <img src={Logo} alt='sums-logo' className='w-28 object-contain'/>
+
+                <button
+                type='button'
+                onClick={() => window.open()}
+                className='balck_btn'
+                >
+                    Github
+                </button>
+            </nav>
+
+            <h1 className='head_text'>
+                Summarize Article with <br className='max-md:hidden'/>
+                <span className='orange _gradient'>OpenAi GPT-4</span>
+            </h1>
+            <h2 className='desc'>
+                simplify your reading with summize,an open-source article summarizer that transforms lengthy articles into clear and concise summaries 
+            </h2>
+        </header>
+    )
+}
